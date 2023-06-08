b2c0375f67f44ad3fe872feb158d03a3ebc928c9
samuel akinboro <victorakinboto2003@gmail.com>
2023-06-08 13:13:32 +0100

    keys

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, SafeAreaView, TouchableOpacity, FlatList, Dimensions } from 'react-native'
+import React from 'react'
 
-export default function App() {
+const {width} = Dimensions.get('window');
+
+const dialpad = [1, 2, 3, 4, 5, 6, 7, 8, 9, '', 0, 'del'];
+const keySize = width * .2;
+const keyTextSize = keySize / 3
+const keyGap = keySize * .2
+
+function DialPad() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <FlatList
+      style={{flexGrow: 0}}
+      data={dialpad}
+      numColumns={3}
+      columnWrapperStyle={{gap: keyGap}}
+      contentContainerStyle={{gap: keyGap}}
+      keyExtractor={(item) => item}
+      scrollEnabled={false}
+      renderItem={({item}) => (
+        <TouchableOpacity
+          style={{
+            width: keySize,
+            height: keySize,
+            borderRadius: keySize,
+            borderWidth: 1,
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+        >
+          <Text style={{
+            fontSize: keyTextSize
+          }}>{item}</Text>
+        </TouchableOpacity>
+      )}
+    />
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      <DialPad />
+    </SafeAreaView>
+  )
+}
+
+export default App
